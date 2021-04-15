e756c8b5c6bbae8542289bb9d9b772f7c09cf6bd
sfus <sfus.dev@gmail.com>
2021-04-15 09:43:04 +0900

    Update sh-mode setting

--- init/prog-langs.el
+++ init/prog-langs.el
@@ -582,14 +582,14 @@
 ;;;; sh-mode
 ;; @eval (find-file-read-only (locate-library "sh-script.el"))
 
-(setq sh-basic-offset 4);; default: 2
-(setq sh-indentation 4) ;; default: 4
+(setq sh-basic-offset 2);; default: 2
+(setq sh-indentation 2) ;; default: 4
 (eval-after-load "sh-script"
   '(define-key sh-mode-map "\C-m" 'newline-and-indent))
 
 (add-hook 'sh-mode-hook
           (lambda ()
-            (setq tab-width 4)
+            (setq tab-width 2)
             ))
 
 (add-to-list 'auto-mode-alist '("/\\.\\(bash\\|zsh\\)rc.*$" . sh-mode))
